8e3ab2a10c2e23e660bef9ace8ea922c28e6abd0
Deborah M Ingabire <ingabineza@gmail.com>
2020-12-26 12:36:02 +0200

    registered models in admin.py

--- galleries/admin.py
+++ galleries/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Photographer,Location,Image,Category
+# Register your models here.
+admin.site.register(Photographer)
+admin.site.register(Location)
+admin.site.register(Image)
+admin.site.register(Category)
