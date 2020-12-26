38ff47c31951814374f6918be97534ac9a24e2b5
Deborah M Ingabire <ingabineza@gmail.com>
2020-12-26 12:41:10 +0200

    added urls in urls.py in galleries

---
+++ galleries/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    url(r'^$',views.welcome,name = 'welcome'),
+    url(r'^search/',  views.search_results,name='search_results'),
+    url(r'^location/(\d+)',  views.filter_by_location,name='location'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
