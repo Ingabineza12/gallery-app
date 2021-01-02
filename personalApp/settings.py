226f2ec8d59c455d3dcfadd506e7dc673b7af4b7
Deborah M Ingabire <ingabineza@gmail.com>
2021-01-02 13:05:38 +0200

    changes

--- personalApp/settings.py
+++ personalApp/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-# import django_on_heroku
-import django_heroku
+import django_on_heroku
+# import django_heroku
 import dj_database_url
 from decouple import config,Csv
 
