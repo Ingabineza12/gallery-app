e89206134608547eca4423fe5138ad6b48d36389
Deborah M Ingabire <ingabineza@gmail.com>
2021-01-02 14:46:32 +0200

    cchanges made

--- personalApp/settings.py
+++ personalApp/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_on_heroku
-# import django_heroku
+import django_heroku
 import dj_database_url
 from decouple import config,Csv
 
@@ -167,4 +166,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
